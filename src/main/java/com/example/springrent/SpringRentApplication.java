d2b77ea14a6a3862df70c4cf3161ac5609cd7474
LopatinBohdan <lopatin.bogdan0405@gmail.com>
2023-09-07 20:21:13 +0300

    Initial commit

---
+++ src/main/java/com/example/springrent/SpringRentApplication.java
@@ -0,0 +1,13 @@
+package com.example.springrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springrent/SpringRentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springrent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
